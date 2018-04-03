a6bd530597d2974bce543c186af34e875f9ad70d
Ryan Dsouza <authenticryanis@gmail.com>
2018-04-03 20:47:09 +0530

    Responsive screen completed. Refresh needed between resizes. Home Screen fully completed

--- java_script/script.js
+++ java_script/script.js
@@ -39,3 +39,15 @@ journal.addEventListener('click', () => {
         journal_count++;
     }
 })
+
+// to change to x for the hamburger icon and its background color
+let ham_count = 0;
+function ham__active() {
+    ham_count++;
+    document.body.classList.toggle("change");
+    if(ham_count %2 == 1){
+        document.getElementsByTagName("nav")[0].style.backgroundColor = "transparent";
+    } else {
+        document.getElementsByTagName("nav")[0].style.backgroundColor = "#fff";        
+    }
+}
