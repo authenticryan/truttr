43f9771bad9203beabe223448f16e19070f36227
Ryan Dsouza <authenticryanis@gmail.com>
2018-04-05 20:57:34 +0530

    Working responsive contact form

--- java_script/script.js
+++ java_script/script.js
@@ -40,14 +40,3 @@ journal.addEventListener('click', () => {
     }
 })
 
-// to change to x for the hamburger icon and its background color
-let ham_count = 0;
-function ham__active() {
-    ham_count++;
-    document.body.classList.toggle("change");
-    if(ham_count %2 == 1){
-        document.getElementsByTagName("nav")[0].style.backgroundColor = "transparent";
-    } else {
-        document.getElementsByTagName("nav")[0].style.backgroundColor = "#fff";        
-    }
-}
